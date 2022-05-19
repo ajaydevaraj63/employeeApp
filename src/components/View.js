3345a6bab938ec2b6876d0d2b73cf96de2ad9e55
ajaydevaraj63 <ajaydvrj@gmail.com>
2022-05-19 10:32:05 +0530

    a

--- src/components/View.js
+++ src/components/View.js
@@ -4,7 +4,39 @@ import Nav from './Nav'
 const View = () => {
   return (
     <div><Nav/>
-          View</div>
+          <table class="table">
+  <thead>
+    <tr>
+     
+      <th scope="col">empid</th>
+      <th scope="col">emp name</th>
+      <th scope="col">designation</th>
+      <th scope="col">salary</th>
+      <th scope="col">company name</th>
+      <th scope="col">dob</th>
+      <th scope="col">email</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+   
+ 
+<tr>
+
+<td>1</td>
+<td>ajay</td>
+<td>hr</td>
+<td>200000</td>
+<td>tcs</td>
+<td>05-07-1999</td>
+<td>ajaydvrj@gmail.com</td>
+</tr>
+
+
+
+   
+  </tbody>
+</table></div>
   )
 }
 
